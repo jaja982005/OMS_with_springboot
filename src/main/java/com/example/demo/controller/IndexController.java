a12175c12ade06cea1f6b9dcb0f8751019dfe47f
jaja982005 <jaja982005@gmail.com>
2024-04-22 09:59:41 +0900

    add the comment

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+    // index page
     @GetMapping("/index")
     public String getMethodName() {
         return "index";
